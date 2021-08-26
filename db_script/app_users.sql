5b71da8f1daf54fc1dd3f5a110f6e2e3ee8c2b28
Deepesh Kumar <deepeshk@dewsolutions.in>
2021-08-26 19:05:25 +0530

    db script added.

---
+++ db_script/app_users.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.20-0ubuntu0.19.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `app_users`
+--
+
+LOCK TABLES `app_users` WRITE;
+/*!40000 ALTER TABLE `app_users` DISABLE KEYS */;
+INSERT INTO `app_users` VALUES (4,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-20 14:35:16','Regional Head','anoop.singh@wemonde.com','9711986743','',NULL,NULL,NULL,'North','Anoop','Singh','',NULL,'vxHjJKq/l9Y=',2,2),(2,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-20 00:24:56','Regional Head','rajeev.pandey@wemonde.com','9911939651','sandeep.gaur@wemonde.com','2021-07-25 00:04:30',NULL,NULL,'North','Rajeev','Ranjan','',NULL,'9GfWbKVkkHuqGAJPbhqLXg==',0,0),(5,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-20 14:42:38','Vertical Head','ankit.sharma@wemonde.com','8802189030','',NULL,NULL,NULL,'North','Ankit ','Sharma','192231','2021-07-27 17:17:21','vxHjJKq/l9Y=',3,4),(6,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 11:42:44','National Sales Head','abc@gmail.com','7991237323','',NULL,NULL,NULL,'North','Shalu','Sabharwal','',NULL,'vxHjJKq/l9Y=',2,2),(7,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 11:47:12','Regional Head ','xyz@gmail.com','7991252652','',NULL,NULL,NULL,'North','Gaurav','Shukla','',NULL,'vxHjJKq/l9Y=',3,6),(8,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:07:43','Vertical  Head','abcd@gmail.com','9898985564','',NULL,NULL,NULL,'North','kunal','Bahal','',NULL,'vxHjJKq/l9Y=',4,7),(9,_binary '',1,'Delhi-NCR',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:15:32','Vertical  Head','ace@gmail.com','8586485214','',NULL,NULL,NULL,'North','Neha','Sharma','',NULL,'vxHjJKq/l9Y=',4,7),(10,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:21:00','AVP','tbc56@gmail.com','9561234565','',NULL,NULL,NULL,'Central','Sharda','ganti','',NULL,'vxHjJKq/l9Y=',3,4),(12,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:26:23','GM','abc.234@gmail.com','8654585263','',NULL,NULL,NULL,'Central','Jaykumar','Mistry','',NULL,'vxHjJKq/l9Y=',4,10),(13,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:27:45','AM ,Sales Accounts','abc00@gmail.com','7854623621','',NULL,NULL,NULL,'Central','Ashok','Jaiswal','',NULL,'vxHjJKq/l9Y=',4,10),(14,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:33:50','senior manager','abcd.34@gmail.com','7566545215','sandeep.gaur@wemonde.com','2021-07-26 10:30:22',NULL,NULL,'Central','Rupal','Arora','',NULL,'vxHjJKq/l9Y=',0,0),(15,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:34:58','sr manager','abc45@gmail.com','7841451245','',NULL,NULL,NULL,'Central','Avinash','korudu','',NULL,'vxHjJKq/l9Y=',5,11),(16,_binary '',2,'Mumbai',1,'INDIA','sandeep.gaur@wemonde.com','2021-07-21 12:40:35','Manager Sales','abc987@gmail.com','8562316456','',NULL,NULL,NULL,'Central','Shuchi','Vijayvergia','',NULL,'vxHjJKq/l9Y=',5,12);
+/*!40000 ALTER TABLE `app_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-26 19:04:10
